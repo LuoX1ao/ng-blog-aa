7ca7ccab669133dcfad9560b046d36492e0c5ec4
LuoX1ao <luoxiaoit@163.com>
2018-01-30 22:53:55 +0800

    top nav item hover change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,13 @@ import { Component } from '@angular/core';
 import {MenuItem} from './layout/menu-item';
 import {MENUS, TOPMENUS} from './layout/menu-config';
 
-
-
 @Component({
   selector: 'app-root',
   template: `
     <app-layout [menus]='menus' [topMenus]='topMenus'>
       <router-outlet></router-outlet>
     </app-layout>
-  `,
-  styleUrls: ['./app.component.css']
+  `
 })
 export class AppComponent {
 
